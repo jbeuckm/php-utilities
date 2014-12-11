7cf969e73c112746ed42f621a306c9efbb18caa7
joe <joseph.beuckman@usbank.com>
2014-12-11 10:21:58 -0600

    added a nest function for arrays

--- php/ArrayHelper.php
+++ php/ArrayHelper.php
@@ -28,4 +28,44 @@ class ArrayHelper
 		}
 		return $r;
 	}
+
+
+  /**
+   * Nest source items into an array of arrays with keys as the unique values of a specified key in the items.
+   *
+   * @param $source array/object The array to extract values from
+   * @param $key string The key of each array item to
+   * @param $extrasKey string Include items from the original array that don't specify the key
+   */
+  public static function nest(array $source, $key, $extrasKey = FALSE)
+  {
+    $result = array();
+    if ($extrasKey) {
+      $result[$extrasKey] = array();
+    }
+
+		foreach ($source as $item) {
+
+      $item = (array) $item;
+
+      if (isset($item[$key])) {
+
+        $val = $item[$key];
+
+        if (!isset($result[$val])) {
+          $result[$val] = array();
+        }
+
+        array_push($result[$val], $item);
+      }
+      elseif ($extrasKey) {
+        array_push($result[$extrasKey], $item);
+      }
+
+    }
+
+    return $result;
+  }
+
+
 }
